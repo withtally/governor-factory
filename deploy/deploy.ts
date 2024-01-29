a25922f4e4a27ec1a05ef9bdc78c42928623db3c
Arthur Abeilice <afa7789@gmail.com>
2024-01-29 12:51:10 -0300

    updating some info at the package json.

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,24 +1,71 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import fs from "fs";
 
-const DAY_IN_SECONDS = 60 * 60 * 24;
-const NOW_IN_SECONDS = Math.round(Date.now() / 1000);
-const UNLOCK_IN_X_DAYS = NOW_IN_SECONDS + DAY_IN_SECONDS * 1; // 1 DAY
+const deployContracts: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const { deploy } = hre.deployments;
+    const { deployer } = await hre.getNamedAccounts();
 
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployer } = await hre.getNamedAccounts();
-  const { deploy } = hre.deployments;
-  const lockedAmount = hre.ethers.parseEther("0.01").toString();
+    console.log("\x1B[37mDeploying ImplementationManager and MockToken contracts");
 
-  const lock = await deploy("Lock", {
-    from: deployer,
-    args: [UNLOCK_IN_X_DAYS],
-    log: true,
-    value: lockedAmount,
-  });
+    // Deploy ImplementationManager contract
+    await (async function deployImplementationManager() {
+        const implementationManager = await deploy("ImplementationManager", {
+            from: deployer,
+            args: [], // ImplementationManager constructor arguments
+            log: true,
+        });
 
-  console.log(`Lock contract: `, lock.address);
+        const implementationManagerBlock = await hre.ethers.provider.getBlock("latest");
+
+        console.log(`\nImplementationManager contract deployed at: `, implementationManager.address);
+
+        // Save to contracts.out file
+        const implementationManagerDeployStr = `ImplementationManager contract deployed at: ${implementationManager.address} - Block number: ${implementationManagerBlock?.number}\n`;
+        fs.appendFileSync("contracts.out", implementationManagerDeployStr);
+
+        // Verify ImplementationManager contract
+        const implementationManagerVerifyStr = `npx hardhat verify --network ${hre.network.name} ${implementationManager.address}\n`;
+        console.log(implementationManagerVerifyStr);
+        fs.appendFileSync("contracts.out", implementationManagerVerifyStr);
+
+    })();
+
+    // Deploy MockToken contract
+    await (async function deployMockToken() {
+        const mockToken = await deploy("MockToken", {
+            from: deployer,
+            args: [], // MockToken constructor arguments
+            log: true,
+        });
+
+        const mockTokenBlock = await hre.ethers.provider.getBlock("latest");
+
+        console.log(`\nMockToken contract deployed at: `, mockToken.address);
+
+        // Save to contracts.out file
+        const mockTokenDeployStr = `MockToken contract deployed at: ${mockToken.address} - Block number: ${mockTokenBlock?.number}\n`;
+        fs.appendFileSync("contracts.out", mockTokenDeployStr);
+
+        // Verify MockToken contract
+        const mockTokenVerifyStr = `npx hardhat verify --network ${hre.network.name} ${mockToken.address}\n`;
+        console.log(mockTokenVerifyStr);
+        fs.appendFileSync("contracts.out", mockTokenVerifyStr);
+    })();
+
+    // Function to add MockToken as an implementation on ImplementationManager
+    async function addMockTokenAsImplementation() {
+        const implementationManager = await hre.deployments.get("ImplementationManager");
+        const mockToken = await hre.deployments.get("MockToken");
+
+        await implementationManager.addImplementation("MockToken", mockToken.address);
+    }
+
+    // Call the function to add MockToken as an implementation on ImplementationManager
+    await addMockTokenAsImplementation();
 };
-export default func;
-func.id = "deploy_lock"; // id required to prevent reexecution
-func.tags = ["Lock"];
+
+deployContracts.id = "deploy_contracts";
+deployContracts.tags = ["ImplementationManager", "MockToken"];
+
+export default deployContracts;
